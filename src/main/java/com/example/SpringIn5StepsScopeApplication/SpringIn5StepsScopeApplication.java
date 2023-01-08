08ba250a2669768d5b5fe5fb752e3c4e2b418dda
Simon Vargas <sjvargas01@gmail.com>
2023-01-08 00:26:47 -0500

    added logger

--- src/main/java/com/example/SpringIn5StepsScopeApplication/SpringIn5StepsScopeApplication.java
+++ src/main/java/com/example/SpringIn5StepsScopeApplication/SpringIn5StepsScopeApplication.java
@@ -1,7 +1,9 @@
 package com.example.SpringIn5StepsScopeApplication;
 
-import com.example.basic.BinarySearchImpl;
+
 import com.example.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +14,9 @@ public class SpringIn5StepsScopeApplication {
     // What are the beans?
     // What are the dependencies of a bean?
     // Where to search for beans? => No need, all beans in same package
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
 
     public static void main(String[] args) {
 
@@ -24,9 +29,16 @@ public class SpringIn5StepsScopeApplication {
 
        ApplicationContext context= SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
 
-       PersonDAO personDAO = context.getBean(PersonDAO.class);
 
+        PersonDAO personDAO = context.getBean(PersonDAO.class);
 
+        PersonDAO personDAO2 = context.getBean(PersonDAO.class);
+
+        LOGGER.info("{}", personDAO);
+        LOGGER.info("{}", personDAO.getJdbcConnection());
+
+        LOGGER.info("{}", personDAO2);
+        LOGGER.info("{}", personDAO2.getJdbcConnection());
 
 
     }
